317a671c003c00bd26a783665fc55bce8e163ab9
Eduardo <eduardomuller989@gmail.com>
2025-02-28 18:45:16 -0400

    teste

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
+import globals from 'globals'
+import pluginJs from '@eslint/js'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     {
         languageOptions: { globals: globals.node },
-        rules: {},
+        rules: {
+            'no-unused-vars': 'off',
+        },
     },
     pluginJs.configs.recommended,
-];
+]
