47e6707f1b865fd2bcb6b1e9f8e7ffa676e98d44
Eduardo <eduardomuller989@gmail.com>
2025-03-01 11:41:51 -0400

    feat: add GetUserByEmailRepository

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,12 +1,23 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
 
 export class CreateUserUseCase {
     constructor() {
         this.postgresCreateUserRepository = new PostgresCreateUserRepository()
+        this.postgresGetUserByEmailRepository =
+            new PostgresGetUserByEmailRepository()
     }
     async execute(createUserParams) {
+        const userWithProviderEmail =
+            await this.postgresGetUserByEmailRepository.execute(
+                createUserParams.email,
+            )
+
+        if (userWithProviderEmail) {
+            throw new Error('Este email ja existe')
+        }
         const userId = uuidv4()
 
         const saltRoads = 10
