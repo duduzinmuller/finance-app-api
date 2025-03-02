31bf023178c5853708cc1ab87127de661244a1d2
Eduardo <eduardomuller989@gmail.com>
2025-03-02 01:48:36 -0400

    feat: add index.js file to controllers and controller helpers

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,6 +1,11 @@
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    serverError,
+    ok,
+    notFound,
+} from './helpers/index.js'
 import { GetUserByIdUseCase } from '../use-cases/get-user-by-id.js'
-import { serverError, ok, notFound } from './helpers/http.js'
-import { checkIfIdIsValid, invalidIdResponse } from './helpers/user.js'
 
 export class GetUserByIdController {
     constructor() {
