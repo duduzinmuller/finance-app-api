dbddb108884aea71c7354acf599b1a8a07320896
Eduardo <eduardomuller989@gmail.com>
2025-03-03 17:09:21 -0400

    feat: add route to get transactions by user id

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -2,7 +2,7 @@ import { PostgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresGetTransactionsByUserIdRepository {
     async execute(userId) {
-        const transactions = await PostgresHelper.execute(
+        const transactions = await PostgresHelper.query(
             'SELECT * FROM transactions WHERE user_id = $1',
             [userId],
         )
