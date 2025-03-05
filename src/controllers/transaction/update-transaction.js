b86ae32d2c65fa32b6c77d676b919ffbac65b905
Eduardo <eduardomuller989@gmail.com>
2025-03-05 14:27:57 -0400

    feat: ensure Update and PostgresGetTransactionsByUserIdRepository use Prisma

--- src/controllers/transaction/update-transaction.js
+++ src/controllers/transaction/update-transaction.js
@@ -1,15 +1,12 @@
+import { ZodError } from 'zod'
 import {
     checkIfIdIsValid,
     invalidIdResponse,
     serverError,
-    badRequest,
-    checkIfAmountIsValid,
-    invalidAmountResponse,
-    checkIfTypeIsValid,
-    invalidTypeResponse,
     ok,
+    badRequest,
 } from '../helpers/index.js'
-
+import { updateTransactionSchema } from '../../schemas/transaction.js'
 export class UpdateTransactionController {
     constructor(updateTransactionUseCase) {
         this.updateTransactionUseCase = updateTransactionUseCase
@@ -24,33 +21,7 @@ export class UpdateTransactionController {
 
             const params = httpRequest.body
 
-            const allowedFields = ['name', 'date', 'amount', 'type']
-
-            const someFieldIsNotAllowed = Object.keys(params).some(
-                (field) => !allowedFields.includes(field),
-            )
-
-            if (someFieldIsNotAllowed) {
-                return badRequest({
-                    message: 'Some provided field is not allowed.',
-                })
-            }
-
-            if (params.amount) {
-                const amountIsValid = checkIfAmountIsValid(params.amount)
-
-                if (!amountIsValid) {
-                    return invalidAmountResponse()
-                }
-            }
-
-            if (params.type) {
-                const typeIsValid = checkIfTypeIsValid(params.type)
-
-                if (!typeIsValid) {
-                    return invalidTypeResponse()
-                }
-            }
+            await updateTransactionSchema.parseAsync(params)
 
             const transaction = await this.updateTransactionUseCase.execute(
                 httpRequest.params.transactionId,
@@ -59,6 +30,11 @@ export class UpdateTransactionController {
 
             return ok(transaction)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
             console.error(error)
             return serverError()
         }
