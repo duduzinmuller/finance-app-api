49c02c228ac9b3b40b5dbb6f12813076b20a600a
Eduardo <eduardomuller989@gmail.com>
2025-03-05 10:43:55 -0400

    feat: add DeleteTransactionUseCase

---
+++ src/use-cases/transaction/delete-transaction.js
@@ -0,0 +1,22 @@
+import { ForbiddenError, TransactionNotFoundError } from '../../errors/user.js'
+
+export class DeleteTransactionUseCase {
+    constructor(deleteTransactionRepository, getTransactionByIdRepository) {
+        this.getTransactionByIdRepository = getTransactionByIdRepository
+        this.deleteTransactionRepository = deleteTransactionRepository
+    }
+
+    async execute(transactionId, userId) {
+        const transaction =
+            await this.getTransactionByIdRepository.execute(transactionId)
+        if (!transaction) {
+            throw new TransactionNotFoundError(transactionId)
+        }
+        if (transaction.user_id !== userId) {
+            throw new ForbiddenError()
+        }
+        const deletedTransaction =
+            await this.deleteTransactionRepository.execute(transactionId)
+        return deletedTransaction
+    }
+}
