2ff6d3cb463d2a3d9d03831938f6b90d8e94b821
Eduardo <eduardomuller989@gmail.com>
2025-03-04 20:51:25 -0400

    feat: add zod to create user controller

---
+++ src/schemas/user.js
@@ -0,0 +1,41 @@
+import { z } from 'zod'
+
+export const createUserSchema = z.object({
+    first_name: z
+        .string({
+            required_error: 'O primeiro nome é obrigatório',
+        })
+        .trim()
+        .min(1, {
+            message: 'O primeiro nome é obrigatório',
+        }),
+    last_name: z
+        .string({
+            required_error: 'O último nome é obrigatório',
+        })
+        .trim()
+        .min(1, {
+            message: 'O último nome é obrigatório',
+        }),
+    email: z
+        .string({
+            required_error: 'O email é obrigatório',
+        })
+        .email({
+            message: 'O email é inválido',
+        })
+        .trim()
+        .min(1, {
+            message: 'O email é obrigatório',
+        }),
+    password: z
+        .string({
+            required_error: 'A senha é obrigatória',
+        })
+        .trim(1, {
+            message: 'A senha é obrigatória',
+        })
+        .min(6, {
+            message: 'A senha deve ter no mínimo 6 caracteres',
+        }),
+})
