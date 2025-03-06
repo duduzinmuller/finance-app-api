17d7c13e6ddc05439a00e79b88b5c6eecbf1ba40
Eduardo <eduardomuller989@gmail.com>
2025-03-06 00:40:06 -0400

    feat: ensure 400 is returned if password is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -107,4 +107,23 @@ describe('Create User Controller', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+    it('should return 400 if password is not provided', async () => {
+        //arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        //act
+        const httpRequest = {
+            body: {
+                first_name: 'Dudu',
+                last_name: 'Ola',
+                email: 'edsadsad',
+            },
+        }
+
+        const result = await createUserController.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
