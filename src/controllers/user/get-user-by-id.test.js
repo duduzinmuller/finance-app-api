a364f2340de0f7393f5f1cfc8a390c72acfeef6f
Eduardo <eduardomuller989@gmail.com>
2025-03-07 13:41:07 -0400

    test: ensure GetUserByIdController returns 500 GetUserByIdUseCase throws

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -53,6 +53,7 @@ describe('GetUserByIdController', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
     it('should return 404 if user is not found', async () => {
         //arrange
         const { sut, getUserByIdUseCase } = makeSut()
@@ -63,5 +64,19 @@ describe('GetUserByIdController', () => {
 
         //assert
         expect(result.statusCode).toBe(404)
+    })
+
+    it('should return 500 if GetUserByIdUseCase throws', async () => {
+        //arrange
+        const { sut, getUserByIdUseCase } = makeSut()
+        jest.spyOn(getUserByIdUseCase, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        //act
+        const result = await sut.execute(httpRequest)
+
+        //assert
+        expect(result.statusCode).toBe(500)
     })
 })
