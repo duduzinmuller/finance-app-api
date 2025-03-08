56149a23405c5ca1d3b36beac44e6db8b3ab6e37
Eduardo <eduardomuller989@gmail.com>
2025-03-08 14:22:28 -0400

    test: ensure GetTransactionsByUserIdController returns 404 when use case throws UserNotFoundError

--- src/controllers/transaction/get-transactions-by-user-id.test.js
+++ src/controllers/transaction/get-transactions-by-user-id.test.js
@@ -1,3 +1,4 @@
+import { UserNotFoundError } from '../../errors/user.js'
 import { GetTransactionsByUserIdController } from './get-transactions-by-user-id.js'
 import { faker } from '@faker-js/faker'
 
@@ -60,5 +61,23 @@ describe('Get Transaction By User ID Controller', () => {
 
         //assert
         expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 404 when GetUserByIdUseCase throws UserNotFoundError', async () => {
+        //arrange
+        const { sut, getTransactionsByUserIdUseCase } = makeSut()
+
+        jest.spyOn(
+            getTransactionsByUserIdUseCase,
+            'execute',
+        ).mockRejectedValueOnce(new UserNotFoundError())
+
+        //act
+        const response = await sut.execute({
+            query: { userId: faker.string.uuid() },
+        })
+
+        //assert
+        expect(response.statusCode).toBe(404)
     })
 })
