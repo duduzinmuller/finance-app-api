c189213b0552066fab0c8174a13b045f278fd9f1
Eduardo <eduardomuller989@gmail.com>
2025-03-08 00:58:58 -0400

    test: ensure UpdateUserController returns 400 if an unllowed field is provided

--- src/controllers/user/update-user.test.js
+++ src/controllers/user/update-user.test.js
@@ -90,4 +90,21 @@ describe('UpdateUserController', () => {
         //assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should 400 when an unllowed field is provided', async () => {
+        //arrange
+        const { sut } = makeSut()
+
+        //act
+        const result = await sut.execute({
+            params: httpRequest.params,
+            body: {
+                ...httpRequest.body,
+                invalidField: 'invalid_value',
+            },
+        })
+
+        //assert
+        expect(result.statusCode).toBe(400)
+    })
 })
