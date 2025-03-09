88db345c95ddf77c77899e6beaf1997c9bda2930
Eduardo <eduardomuller989@gmail.com>
2025-03-08 21:57:33 -0400

    test: ensure GetUserBalanceController returns 404 if use case throws UserNotFoundError

--- src/controllers/user/get-user-balance.test.js
+++ src/controllers/user/get-user-balance.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { GetUserBalanceController } from './get-user-balance.js'
+import { UserNotFoundError } from '../../errors/user.js'
 
 describe('GetUserBalanceController', () => {
     class GetUserBalanceUseCaseStub {
@@ -66,5 +67,19 @@ describe('GetUserBalanceController', () => {
 
         //assert
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.params.userId)
+    })
+
+    it('should return 404 if GetUserBalance throws UserNotFoundError', async () => {
+        //arrange
+        const { sut, getUserBalanceUseCase } = makeSut()
+        jest.spyOn(getUserBalanceUseCase, 'execute').mockRejectedValueOnce(
+            new UserNotFoundError(),
+        )
+
+        //act
+        const response = await sut.execute(httpRequest)
+
+        //assert
+        expect(response.statusCode).toBe(404)
     })
 })
