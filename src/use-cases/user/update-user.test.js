6e94b3cb9a2170e30fb47911a44b6f69ecdb9664
Eduardo <eduardomuller989@gmail.com>
2025-03-09 16:55:21 -0400

    test: ensure UpdateUserUseCase updated successfully (with password)

--- src/use-cases/user/update-user.test.js
+++ src/use-cases/user/update-user.test.js
@@ -78,4 +78,20 @@ describe('UpdateUserUseCase', () => {
         expect(executeSpy).toHaveBeenCalledWith(email)
         expect(result).toBe(user)
     })
+
+    it('should update user successfully (with password)', async () => {
+        //arrange
+        const { sut, passwordHasherAdapter } = makeSut()
+        const executeSpy = jest.spyOn(passwordHasherAdapter, 'execute')
+        const password = faker.internet.password()
+
+        //act
+        const result = await sut.execute(faker.string.uuid(), {
+            password,
+        })
+
+        //assert
+        expect(executeSpy).toHaveBeenCalledWith(password)
+        expect(result).toBe(user)
+    })
 })
