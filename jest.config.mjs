ae523f778c096dcf505c89c5b4073fc0c79c5dad
Eduardo <eduardomuller989@gmail.com>
2025-03-10 13:59:52 -0400

    chore: add jest global setup file to run test db before all tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,7 @@ const config = {
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     collectCoverageFrom: ['src/**/*.js'],
+    globalSetup: '<rootDir>/jest.global-setup.mjs',
 }
 
 export default config
