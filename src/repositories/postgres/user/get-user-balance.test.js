ceb60581272a0a1451de9e404ef5fab1d17cdcee
Eduardo <eduardomuller989@gmail.com>
2025-03-12 16:13:55 -0400

    test: ensure PostgresGetUserBalanceRepository throws if Prisma throws

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -102,4 +102,16 @@ describe('PostgresGetUserBalanceRepository', () => {
             },
         })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresGetUserBalanceRepository()
+
+        jest.spyOn(prisma.transaction, 'aggregate').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const promise = sut.execute(fakeUser.id)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
