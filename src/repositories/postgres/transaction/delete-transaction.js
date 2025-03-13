4650c8883734b949c3377e58065a5f3da443a016
Eduardo <eduardomuller989@gmail.com>
2025-03-13 15:16:29 -0400

    fix: added two new tests for delete one for deleting a transaction

--- src/repositories/postgres/transaction/delete-transaction.js
+++ src/repositories/postgres/transaction/delete-transaction.js
@@ -1,4 +1,4 @@
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library.js'
 import { prisma } from '../../../../prisma/prisma.js'
 import { TransactionNotFoundError } from '../../../errors/index.js'
 
@@ -12,8 +12,9 @@ export class PostgresDeleteTransactionRepository {
             })
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 = "An operation failed because it depends on one or more records that were required but not found" (from Prisma docs)
                 if (error.code === 'P2025') {
-                    throw new TransactionNotFoundError()
+                    throw new TransactionNotFoundError(transactionId)
                 }
             }
 
