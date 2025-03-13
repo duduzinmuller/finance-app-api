f31558988865a54b2336d78d7a2a282ee20c2bf2
Eduardo <eduardomuller989@gmail.com>
2025-03-13 15:58:46 -0400

    test: ensure PostgresDeleteUserRepository returns 404 if UserNotFoundError throws

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { DeleteUserController } from './delete-user.js'
 import { user } from '../../tests/index.js'
+import { UserNotFoundError } from '../../errors/user.js'
 
 describe('DeleteUserController', () => {
     class DeleteUserUseCaseStub {
@@ -45,7 +46,9 @@ describe('DeleteUserController', () => {
     it('should return 404 if user is not found', async () => {
         //arrange
         const { sut, deleteUserUseCase } = makeSut()
-        jest.spyOn(deleteUserUseCase, 'execute').mockReturnValueOnce(null)
+        jest.spyOn(deleteUserUseCase, 'execute').mockRejectedValueOnce(
+            new UserNotFoundError(),
+        )
 
         //act
         const result = await sut.execute(httpRequest)
