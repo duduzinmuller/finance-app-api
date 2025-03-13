467747ff5dace6a541807189ea760e1bf02240db
Eduardo <eduardomuller989@gmail.com>
2025-03-13 17:41:56 -0400

    refactor: move user routes to separate file

---
+++ src/routes/users.js
@@ -0,0 +1,50 @@
+import { Router } from 'express'
+import {
+    makeCreateUserController,
+    makeDeleteUserController,
+    makeGetUserBalanceController,
+    makeGetUserByIdController,
+    makeUpdateUserController,
+} from '../factories/controllers/user.js'
+
+export const userRouter = Router()
+
+userRouter.get('/:userId', async (request, response) => {
+    const getUserByIdController = makeGetUserByIdController()
+
+    const { statusCode, body } = await getUserByIdController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+userRouter.get('/:userId/balance', async (request, response) => {
+    const getUserBalanceController = makeGetUserBalanceController()
+
+    const { statusCode, body } = await getUserBalanceController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+userRouter.post('/', async (request, response) => {
+    const createUserController = makeCreateUserController()
+
+    const { statusCode, body } = await createUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+userRouter.patch('/:userId', async (request, response) => {
+    const updateUserController = makeUpdateUserController()
+
+    const { statusCode, body } = await updateUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
+
+userRouter.delete('/:userId', async (request, response) => {
+    const deleteUserController = makeDeleteUserController()
+
+    const { statusCode, body } = await deleteUserController.execute(request)
+
+    response.status(statusCode).send(body)
+})
