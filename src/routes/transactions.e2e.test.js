2e4045a3c66f211a394ec7627ece2f0ed10a783d
Eduardo <eduardomuller989@gmail.com>
2025-03-13 22:14:48 -0400

    test: ensure DELETE /api/transactions/:id return 404 when updating a non-existing transaction

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -89,4 +89,12 @@ describe('Transaction Routes E2E Tests', () => {
 
         expect(response.status).toBe(404)
     })
+
+    it('DELETE/api/transactions/:transactionId should return 404 when updating a non-existing transaction', async () => {
+        const response = await request(app).delete(
+            `/api/transactions/${transaction.id}`,
+        )
+
+        expect(response.status).toBe(404)
+    })
 })
