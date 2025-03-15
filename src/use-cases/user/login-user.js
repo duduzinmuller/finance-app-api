b11e0e3de01aba56f9eddf336b74b41f015e0f3d
Eduardo <eduardomuller989@gmail.com>
2025-03-15 00:26:20 -0400

    feat: add LoginUserUseCase

---
+++ src/use-cases/user/login-user.js
@@ -0,0 +1,32 @@
+import { InvalidPasswordError, UserNotFoundError } from '../../errors/index.js'
+
+export class LoginUserUseCase {
+    constructor(
+        getUserByEmailRepository,
+        passwordComparatorAdapter,
+        tokensGeneratorAdapter,
+    ) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.passwordComparatorAdapter = passwordComparatorAdapter
+        this.tokensGeneratorAdapter = tokensGeneratorAdapter
+    }
+    async execute(email, password) {
+        const user = await this.getUserByEmailRepository.execute(email)
+        if (!user) {
+            throw new UserNotFoundError()
+        }
+
+        const isPasswordValid = this.passwordComparatorAdapter.execute(
+            password,
+            user.password,
+        )
+        if (!isPasswordValid) {
+            throw new InvalidPasswordError()
+        }
+
+        return {
+            ...user,
+            tokens: this.tokensGeneratorAdapter.execute(user.id),
+        }
+    }
+}
