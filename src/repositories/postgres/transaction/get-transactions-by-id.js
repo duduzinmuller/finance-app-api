f4569b93534367802413943734f8abd0a154267f
Eduardo <eduardomuller989@gmail.com>
2025-03-19 11:27:47 -0400

    feat: ensure user can only delete his transactions

--- src/repositories/postgres/transaction/get-transactions-by-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-id.js
@@ -2,7 +2,7 @@ import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresGetTransactionByIdRepository {
     async execute(transactionId) {
-        return await prisma.transaction.findMany({
+        return await prisma.transaction.findUnique({
             where: {
                 id: transactionId,
             },
